ff101b7afff305e4a278f79a46fdc43c5add7c39
DjallelZ <zerroug.djallel@gmail.com>
2021-02-01 17:43:16 +0100

    Outlets et méthode prepare pour envoyer la difficulté à ListViewController créés.

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -9,5 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var boutonFacile: UIButton!
+    
+    @IBOutlet weak var boutonMoyen: UIButton!
+    
+    @IBOutlet weak var boutonDifficile: UIButton!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let boutonEmetteur: UIButton = sender as! UIButton
+        var difficulteEmise = ""
+        
+        if boutonEmetteur.currentTitle == "Facile" {
+            difficulteEmise = "Facile"
+        } else if boutonEmetteur.currentTitle == "Moyen" {
+            difficulteEmise = "Moyen"
+        } else if boutonEmetteur.currentTitle == "Difficile" {
+            difficulteEmise = "Difficile"
+        }
+        
+        if segue.destination is ListViewController {
+            let viewController = segue.destination as? ListViewController
+            viewController?.difficulte = difficulteEmise
+        }
+    }
 }
 
