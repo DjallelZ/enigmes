1e4020e78082d03f865b1ffabba90943dc205e6f
DjallelZ <zerroug.djallel@gmail.com>
2021-02-04 01:59:30 +0100

    Classe Enigme créée

---
+++ Enigme.swift
@@ -0,0 +1,16 @@
+class Enigme {
+    // Attributs
+    var titre: String = ""
+    var texte: String = ""
+    var reponse: String = ""
+    var explication: String = ""
+    
+    // Constructeur
+    init(titre: String, texte: String, reponse: String, explication: String) {
+        self.titre = titre
+        self.texte = texte
+        self.reponse = reponse
+        self.explication = explication
+    }
+    
+}
