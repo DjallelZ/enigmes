4bbdb3e4d5b51f1c680f06156408486be70b0914
DjallelZ <zerroug.djallel@gmail.com>
2021-02-06 00:23:55 +0100

    Class Enigm modifié avec de nouveaux attributs + interface Codable

--- Enigme.swift
+++ Enigme.swift
@@ -1,16 +1,20 @@
-class Enigme {
+class Enigme: Codable {
     // Attributs
     var titre: String = ""
     var texte: String = ""
     var reponse: String = ""
     var explication: String = ""
+    var difficulte: String = ""
+    var statut: String = ""
     
     // Constructeur
-    init(titre: String, texte: String, reponse: String, explication: String) {
+    init(titre: String, texte: String, reponse: String, explication: String, difficulte: String, statut: String) {
         self.titre = titre
         self.texte = texte
         self.reponse = reponse
         self.explication = explication
+        self.difficulte = difficulte
+        self.statut = statut
     }
     
 }
