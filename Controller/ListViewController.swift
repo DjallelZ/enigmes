299d866482c0b10cef7ec7bba082c1a4c085fb1f
DjallelZ <zerroug.djallel@gmail.com>
2021-02-07 01:32:31 +0100

    Commentaires et corrections

--- Controller/ListViewController.swift
+++ Controller/ListViewController.swift
@@ -24,17 +24,15 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     // Récupération de la liste d'énigme depuis l'instance unique de EnigmeService en fonction de la difficulte choisie
     lazy var enigmes: [Enigme] = EnigmeService.serviceEnigme.recupererListeEnigmesParDifficulte(difficulte: difficulteChoisie)
     
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        
-    }
-    
+    // Génère le nombre de lignes de la tableView en fonction du nombre d'énigmes
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return enigmes.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Déclaration d'une cellule en utilisant notre classe perso "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as! Cell
-        
+        // Affectation du titre du bouton/cellule à partir du titre de l'enigme
         cell.enigmeTitreBouton.setTitle(enigmes[indexPath.row].titre, for: .normal)
         
         return cell
@@ -42,12 +40,11 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     // Préparation de l'énigme à transmettre à la view contenant la liste des enigmes
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         // Cast du paramètre Any? afin de pouvoir manipuler les attributs de la classe UIButton
         let boutonEmetteur: UIButton = sender as! UIButton
-        // objet énigme à envoyer à destination de la view l'affichant
+        // Objet énigme à envoyer à destination de la view l'affichant
         let enigmeEmise:Enigme = EnigmeService.serviceEnigme.recupererEnigmeViaTitre(titre: boutonEmetteur.currentTitle!)
-    
+        // Si la View destinataire est bien celle qu'il faut, on lui envoie une variable contenant l'objet Enigme
         if segue.destination is EnigmeViewController {
             let viewController = segue.destination as? EnigmeViewController
             viewController?.enigme = enigmeEmise
